5f64b86b45b195f5ae9cd98bbc056ed155442995
tixsalvador <tix.salvador@gmail.com>
2013-09-18 00:05:23 -0400

    automove1.02.ino

---
+++ automove1.02.ino
@@ -0,0 +1,130 @@
+/*
+091513	Configure Ultrasonic Sensor 
+091613	Configure bot movement relative to encoder pair
+091813	Added bot Uturn
+*/
+
+#define leftFmotor 6 //M1 Speed Control 
+#define rightFmotor 5 //M2 Speed Control
+#define leftRmotor 8 //M1 Direction Control
+#define rightRmotor 7 //M2 Direction Control
+#define distancesensorPin 3
+int leftmotorSpeed=200;
+int rightmotorSpeed=200;
+long count = 0;
+int rawsensorData = 0;
+int sensorData1= 0;
+int sensorData0 = 0;
+boolean movingForward=false;
+boolean crash=false;
+int crashzone=5; //Value in centimiters
+
+void setup()
+{
+	Serial.begin(9600);
+	pinMode(distancesensorPin, OUTPUT);
+	for(int i=5; i<=8; i++) {
+		pinMode(i, OUTPUT);
+	}
+}
+
+void loop()
+{
+	int x=((encoderCount()) - 10);
+	int uturn=((encoderCount()) + 25);
+        if((getDistance()) < crashzone) {
+                crash=true;
+        }
+        if(crash == false) {
+                forward();
+        }
+        if(crash == true) {
+			do {
+                	reverse();
+			}	
+			while((encoderCount()) >  x);
+			do {
+			right();
+			}
+			while((encoderCount()) < uturn);
+			crash=false;
+        }	
+        Serial.println(encoderCount());
+}
+
+void stop()
+{
+	digitalWrite(leftFmotor, LOW);
+        digitalWrite(rightFmotor, LOW);
+        digitalWrite(leftRmotor, LOW);
+        digitalWrite(rightRmotor, LOW);
+}
+
+void forward()
+{		
+	analogWrite(leftFmotor, leftmotorSpeed);
+		analogWrite(rightFmotor, rightmotorSpeed);
+		digitalWrite(leftRmotor, LOW);
+		digitalWrite(rightRmotor, LOW);
+}
+
+void reverse()
+{
+    analogWrite(leftFmotor, leftmotorSpeed);
+    analogWrite(rightFmotor, rightmotorSpeed);
+    digitalWrite(leftRmotor, HIGH);
+    digitalWrite(rightRmotor, HIGH);
+}
+
+void right()
+{
+	analogWrite(leftFmotor, leftmotorSpeed);
+    	analogWrite(rightFmotor, rightmotorSpeed);
+    	digitalWrite(leftRmotor, LOW);
+    	digitalWrite(rightRmotor, HIGH);
+}
+
+int encoderCount()
+{
+	rawsensorData=analogRead(0);
+	if(rawsensorData < 600 && (digitalRead(leftRmotor) == LOW || digitalRead(rightRmotor) == LOW)) {
+            movingForward=true;
+            sensorData1 = 1;
+    }
+    else if(rawsensorData < 600 && digitalRead(leftRmotor) == HIGH && digitalRead(rightRmotor) == HIGH) {
+            movingForward=false;
+            sensorData1 = 1;
+    }
+    else {
+    		sensorData1 = 0;
+    }
+    
+    if(sensorData1 != sensorData0 && movingForward == true) {
+            count ++;
+    }
+    else if (sensorData1 != sensorData0 && movingForward == false) {
+      		count --;
+    }
+    sensorData0 = sensorData1;
+    return count;
+}
+
+int getDistance()
+{
+        int distance;
+        unsigned long pulse =0;
+
+        pinMode(distancesensorPin, OUTPUT);
+        digitalWrite(distancesensorPin, LOW);
+        delayMicroseconds(5);
+
+        digitalWrite(distancesensorPin, HIGH);
+        delayMicroseconds(5);
+        digitalWrite(distancesensorPin, LOW);
+
+        pinMode(distancesensorPin, INPUT);
+        pulse=pulseIn(distancesensorPin, HIGH);
+        pulse=pulse/2;
+        distance = int(pulse/29);
+        return distance;
+}
