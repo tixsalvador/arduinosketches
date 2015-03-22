f6f103c42a079e4f926a47efc526e00af9e1dd02
tixsalvador <tix.salvador@gmail.com>
2015-03-21 21:22:28 -0400

    Added more sensors

--- Arduino_Master.ino
+++ Arduino_Master.ino
@@ -3,10 +3,13 @@
 #include<Wire.h>
 
 byte dataAvailable;
+int dataSize=12;
 int Xaxis;
 int Yaxis;
 int Zaxis;
 float voltage;
+float leftMotorCurrent;
+float rightMotorCurrent;
 
 void setup()
 {
@@ -16,18 +19,28 @@ void setup()
 
 void trex_Sensor_Values()
 {
-	dataAvailable=Wire.requestFrom(0x07,6);
-	if(dataAvailable==6){
+	dataAvailable=Wire.requestFrom(0x07,dataSize);
+	if(dataAvailable==dataSize){
 		Xaxis=Wire.read()<<8|Wire.read();
 		Yaxis=Wire.read()<<8|Wire.read();
 		Zaxis=Wire.read()<<8|Wire.read();
-	//	voltage=Wire.read()<<8|Wire.read();
-	//	voltage=voltage/33.57;
+		voltage=Wire.read()<<8|Wire.read();
+		voltage=voltage/33.57;
+		leftMotorCurrent=Wire.read()<<8|Wire.read();
+		leftMotorCurrent=((leftMotorCurrent-511)*48.83)/1000;
+		rightMotorCurrent=Wire.read()<<8|Wire.read();
+		rightMotorCurrent=((rightMotorCurrent-511)*48.83)/1000;
 		Serial.print(Xaxis);
 		Serial.print("\t");
 		Serial.print(Yaxis);
 		Serial.print("\t");
-		Serial.println(Zaxis);
+		Serial.print(Zaxis);
+		Serial.print("\t");
+		Serial.print(voltage);
+		Serial.print("\t");
+		Serial.print(leftMotorCurrent);
+		Serial.print("\t");
+		Serial.println(rightMotorCurrent);
 	}
 	else {
 		Serial.println("ERROR");
