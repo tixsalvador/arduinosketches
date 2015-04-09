3dc086b75c57028110cce44edbab0f311a6864bf
tixsalvador <tix.salvador@gmail.com>
2015-04-08 22:09:09 -0400

    GPS test

--- GPS.ino
+++ GPS.ino
@@ -19,10 +19,26 @@ void loop()
 		a=sS.read();
 		if(gps.encode(a)){
 			float latitude,longitude;
+			int year;
+			byte month,day,hour,minutes,second,hundredths;
+			gps.crack_datetime(&year,&month,&day,&hour,&minutes,&second,&hundredths);
+
 			gps.f_get_position(&latitude,&longitude);
-			Serial.print(latitude,5);
+			Serial.print(latitude,8);
+                        Serial.print("\t");
+                        Serial.print(longitude,8);
+                        Serial.print("\t");
+			Serial.print(year);
 			Serial.print("\t");
-			Serial.println(longitude,5);
+			Serial.print(month);
+			Serial.print("\t");
+			Serial.print(day);
+                        Serial.print("\t");
+                        Serial.print(hour);
+			Serial.print("\t");
+                        Serial.print(minutes);
+                        Serial.print("\t");
+                        Serial.println(second);
 		}
 	}
 }
