46139134ba0d628ef859341a49450fd9e3bbd623
tixsalvador <tix.salvador@gmail.com>
2015-04-25 11:57:06 -0400

    Added longitude value

--- I2C_Gps_master.ino
+++ I2C_Gps_master.ino
@@ -2,11 +2,18 @@
 
 union lat2bytes
 {
-	float f;
-	char b[sizeof(float)];
+	float latf;
+	char latb[sizeof(float)];
+};
+
+union lon2bytes
+{
+	float lonf;
+	char lonb[sizeof(float)];
 };
 
 float latitude;
+float longitude;
 
 void setup()
 {
@@ -17,14 +24,22 @@ void setup()
 void loop()
 {
 	lat2bytes la2b;
-	Wire.requestFrom(7,4);
+	lon2bytes lo2b;
+	Wire.requestFrom(2,8);
 	while(Wire.available())
 	{
-		la2b.b[0]=Wire.read();
-		la2b.b[1]=Wire.read();
-		la2b.b[2]=Wire.read();
-		la2b.b[3]=Wire.read();
-		latitude=la2b.f;
+		la2b.latb[0]=Wire.read();
+		la2b.latb[1]=Wire.read();
+		la2b.latb[2]=Wire.read();
+		la2b.latb[3]=Wire.read();
+		latitude=la2b.latf;
+		lo2b.lonb[0]=Wire.read();
+		lo2b.lonb[1]=Wire.read();
+		lo2b.lonb[2]=Wire.read();
+		lo2b.lonb[3]=Wire.read();
+		longitude=lo2b.lonf;
 	}
-	Serial.println(latitude,6);
+	Serial.print(latitude,6);
+	Serial.print("\t");
+	Serial.println(longitude,6);
 }
