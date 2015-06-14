26ebfa41e5b53df3c4acb58203e170f88b3ffa27
tixsalvador <tix.salvador@gmail.com>
2015-06-13 21:03:27 -0400

    Fix movement

--- Arduino_Master.ino
+++ Arduino_Master.ino
@@ -1,16 +1,10 @@
-//Arduino MASTER
+//Arduino MASTER_MANUAL
 
 #include <Wire.h>
-#include <SoftwareSerial.h>
-#include <TinyGPS++.h>
-#include <SD.h>
+#include <math.h>
+#include <LiquidCrystal_I2C.h>
 
-SoftwareSerial sS(2,3);
-TinyGPSPlus gps;
-
-File myFile;
-
-const int sdCard=10;
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 byte I2Caddress=0x07;
 byte dataAvailable;
@@ -21,6 +15,8 @@ int Zaxis;
 float voltage;
 float leftMotorCurrent;
 float rightMotorCurrent;
+
+double x,y,z;
 
 byte start;
 byte buffer[9];
@@ -36,73 +32,33 @@ int rightMotorSpeed;
 unsigned int lSpeed=255;
 unsigned int rSpeed=255;
 
-unsigned long previous_Time=0;
+byte alternate;
+unsigned long previous_Time_Data=0;
+
+unsigned long previous_Time_Xbee=0;
+
+union lat2bytes
+{
+	float latf;
+	char latb[sizeof(float)];
+};
+
+union lon2bytes
+{
+	float lonf;
+	char lonb[sizeof(float)];
+};
+
+float latitude;
+float longitude;
 
 void setup()
 {
 	Serial.begin(9600);
-	sS.begin(9600);
 	Wire.begin();
-	pinMode(6,OUTPUT);
-	pinMode(10,OUTPUT);
-	digitalWrite(6,LOW);
-	check_SDCard();
-}
-
-void check_SDCard()
-{
-	if(!SD.begin(sdCard)){
-		Serial.println("Card not Present");
-		return;
-	}
-	if(!SD.exists("gpsLog.txt")){
-		myFile=SD.open("gpsLog.txt",FILE_WRITE);
-		myFile.close();
-		Serial.println("Created File");
-	}
-	myFile=SD.open("gpsLog.txt",FILE_WRITE);
-	if(myFile){
-		if(myFile.size()>1000000){
-			SD.remove("gpsLog.txt");
-			myFile=SD.open("gpsLog.txt",FILE_WRITE);
-			myFile.close();
-		}
-	}
-}
-
-
-void gps_Stats()
-{
-	while(sS.available()>0){
-		gps.encode(sS.read());
-		if(gps.location.isUpdated()){
-			myFile=SD.open("gpsLog.txt",FILE_WRITE);
-			if(myFile){
-				myFile.print(gps.date.value());
-				myFile.print(", ");
-				myFile.print(gps.time.value());
-				myFile.print(", ");
-				myFile.print(gps.altitude.value());
-				myFile.print(", ");
-				myFile.print(gps.speed.mps());
-				myFile.print(", ");
-				myFile.print(gps.location.lat());
-				myFile.print(", ");
-				myFile.print(gps.location.lng());
-				myFile.print(", ");
-				myFile.println(gps.satellites.value());
-				myFile.close();
-			}
-		}
-/*
-		myFile=SD.open("gpsLog.txt");
-		if(myFile){
-			while(myFile.available()){
-				Serial.write(myFile.read());
-			}myFile.close();
-		}
-*/
-	}
+	lcd.init();
+	lcd.init();
+	lcd.backlight();
 }
 
 void trex_Sensor_Values()
@@ -118,7 +74,8 @@ void trex_Sensor_Values()
 		leftMotorCurrent=((leftMotorCurrent-511)*48.83)/1000;
 		rightMotorCurrent=Wire.read()<<8|Wire.read();
 		rightMotorCurrent=((rightMotorCurrent-511)*48.83)/1000;
-/*		Serial.print(Xaxis);
+/*
+		Serial.print(Xaxis);
 		Serial.print("\t");
 		Serial.print(Yaxis);
 		Serial.print("\t");
@@ -128,73 +85,13 @@ void trex_Sensor_Values()
 		Serial.print("\t");
 		Serial.print(leftMotorCurrent);
 		Serial.print("\t");
-		Serial.println(rightMotorCurrent); */
+		Serial.println(rightMotorCurrent);
+*/
 	}
 	else {
 		Serial.println("ERROR");
 	}
 }
-
-void stop()
-{
-	leftMotorBreak=1;
-        rightMotorBreak=1;
-        leftMotorDir=1;
-        rightMotorDir=1;
-        leftMotorSpeed=0;
-        rightMotorSpeed=0;
-        trex_Send_Data();
-}
-
-void backward()
-{
-	leftMotorBreak=0;
-	rightMotorBreak=0;
-	leftMotorDir=1;
-	rightMotorDir=1;
-	leftMotorSpeed=lSpeed;
-	rightMotorSpeed=rSpeed;
-	trex_Send_Data();
-	
-}
-
-
-void forward()
-{
-        leftMotorBreak=0;
-        rightMotorBreak=0;
-        leftMotorDir=0;
-        rightMotorDir=0;
-        leftMotorSpeed=lSpeed;
-        rightMotorSpeed=rSpeed;
-        trex_Send_Data();
-
-}
-
-void right()
-{
-        leftMotorBreak=0;
-        rightMotorBreak=0;
-        leftMotorDir=0;
-        rightMotorDir=1;
-        leftMotorSpeed=150;
-        rightMotorSpeed=150;
-        trex_Send_Data();
-
-}
-
-void left()
-{
-        leftMotorBreak=0;
-        rightMotorBreak=0;
-        leftMotorDir=1;
-        rightMotorDir=0;
-        leftMotorSpeed=150;
-        rightMotorSpeed=150;
-        trex_Send_Data();
-
-}
-
 
 void trex_Send_Data()
 {
@@ -216,37 +113,117 @@ void trex_Send_Data()
 
 void xBee_Control()
 {
-		char x=Serial.read();
-		if(x=='w'){
-			forward();
-		}
-		else if(x=='s'){
-			backward();
-		}
-		else if(x=='a'){
-			left();
-		}
-		else if(x=='d'){
-			right();
-		}
-		else {
-			stop();
-		}
+	char x=Serial.read();
+	Wire.beginTransmission(I2Caddress);
+    Wire.write(x);
+    Wire.endTransmission();
+}
+
+void forward()
+{
+	Wire.beginTransmission(I2Caddress);
+	Wire.write('w');
+	Wire.endTransmission();
+}
+
+void stop()
+{
+        Wire.beginTransmission(I2Caddress);
+        Wire.write('t');
+        Wire.endTransmission();
+}
+
+void check_angle_magnitude()
+{
+	int min=529;
+	int max=724;
+	int xAng=map(Xaxis,min,max,-90,90);
+	int yAng=map(Yaxis,min,max,-90,90);
+	int zAng=map(Zaxis,min,max,-90,90);
+	x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  	y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+ 	z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
+/*	
+	Serial.print("x: ");
+  	Serial.print(x);
+	Serial.print(" | y: ");
+  	Serial.print(y);
+  	Serial.print(" | z: ");
+  	Serial.println(z);
+*/
+}
+
+void get_GPS_data()
+{
+	lat2bytes la2b;
+	lon2bytes lo2b;
+	Wire.requestFrom(6,8);
+	while(Wire.available())
+	{
+		la2b.latb[0]=Wire.read();
+		la2b.latb[1]=Wire.read();
+		la2b.latb[2]=Wire.read();
+		la2b.latb[3]=Wire.read();
+		latitude=la2b.latf;
+		lo2b.lonb[0]=Wire.read();
+		lo2b.lonb[1]=Wire.read();
+		lo2b.lonb[2]=Wire.read();
+		lo2b.lonb[3]=Wire.read();
+		longitude=lo2b.lonf;
+	}
+	Serial.print(latitude,6);
+	Serial.print("\t");
+	Serial.println(longitude,6);
+}
+
+void lcd_Gps_Angle()
+{
+	lcd.setCursor(0,0);
+        lcd.print("Latitude:");
+        lcd.setCursor(9,0);
+        lcd.print(latitude,6);
+        lcd.setCursor(0,1);
+        lcd.print("Longitude:");
+        lcd.setCursor(10,1);
+        lcd.print(longitude,6);
+	lcd.setCursor(0,2);
+	lcd.print("x:");
+	lcd.setCursor(2,2);
+	lcd.print(x);
+	lcd.setCursor(11,2);
+	lcd.print("y:");
+	lcd.setCursor(13,2);
+	lcd.print(y);
+	lcd.setCursor(0,3);
+	lcd.print("z:");
+	lcd.setCursor(2,3);
+	lcd.print(y);
+	lcd.setCursor(11,3);
+	lcd.print("V:");
+	lcd.setCursor(13,3);
+	lcd.print(voltage);
 }
 
 void loop()
 {
 	delay(100);
-//	trex_Sensor_Values();
-	gps_Stats();
+	trex_Sensor_Values();
 	while(voltage > 6.50){
-		unsigned int timeDelay=5000;
-		if(millis()-previous_Time>=timeDelay){
-			trex_Sensor_Values();
-			previous_Time=millis();
+		unsigned int timeDelay=500;
+		if(millis()-previous_Time_Data>=timeDelay){
+			alternate=alternate^1;
+			previous_Time_Data=millis();
+			if(alternate){
+				trex_Sensor_Values();
+				check_angle_magnitude();
+			}
+			else{
+				get_GPS_data();
+			}
 		}
 		if(Serial.available()>0){
 			xBee_Control();
 		}
-	}
+		lcd_Gps_Angle();
+	} Serial.println("Battery charge low");
 }
