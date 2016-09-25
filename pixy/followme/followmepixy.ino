f6499b1d5b0005519b973c07f35a8ff9c23d53b5
Tix <tix.salvador@gmail.com>
2016-09-24 20:38:01 -0400

    Added sonar reading

--- pixy/followme/followmepixy.ino
+++ pixy/followme/followmepixy.ino
@@ -1,28 +1,22 @@
 #include <SPI.h>
 #include <Pixy.h>
 
-#define X_CENTER	((PIXY_MAX_X-PIXY_MIN_X)/2) 	
-#define Y_CENTER 	((PIXY_MAX_Y-PIXY_MIN_Y)/2)
+#define X_CENTER        ((PIXY_MAX_X-PIXY_MIN_X)/2)
+#define Y_CENTER        ((PIXY_MAX_Y-PIXY_MIN_Y)/2)
 
 Pixy pixy;
 
 class ServoLoop
 {
 public:
-	ServoLoop(int32_t pgain, int32_t dgain);
+        ServoLoop(int32_t pgain, int32_t dgain);
 
-	int32_t m_pos;
-	int32_t m_prevError;
-	int32_t m_pgain;
-	int32_t m_dgain;
-	
-	void update()
-	{
-		Serial.print(m_pgain);
-		Serial.print("\t");
-		Serial.println(m_dgain);
-	}
-		
+        void update(int32_t error);
+
+        int32_t m_pos;
+        int32_t m_prevError;
+        int32_t m_pgain;
+        int32_t m_dgain;
 };
 
 ServoLoop panLoop(300,500);
@@ -30,19 +24,79 @@ ServoLoop tiltLoop(500,700);
 
 ServoLoop::ServoLoop(int32_t pgain, int32_t dgain)
 {
-	m_pgain = pgain;
-	m_dgain = dgain;
+        m_pos = PIXY_RCS_CENTER_POS;
+        m_prevError = 0x80000000L;
+        m_pgain = pgain;
+        m_dgain = dgain;
+}
+
+void ServoLoop::update(int32_t error)
+{
+        long int vel;
+        if(m_prevError!=0x80000000){
+                vel = (error * m_pgain + (error - m_prevError) * m_dgain) >> 10;
+                m_pos += vel;
+                if(m_pos>PIXY_RCS_MAX_POS){
+                        m_pos=PIXY_RCS_MAX_POS;
+                }
+                else if(m_pos<PIXY_RCS_MIN_POS){
+                        m_pos=PIXY_RCS_MIN_POS;
+                }
+        }
+        m_prevError = error;
 }
 
 void setup()
 {
-	Serial.begin(9600);
-	panLoop.update();
-	tiltLoop.update();
+        Serial.begin(9600);
+        pixy.init();
+}
+
+void track_object()
+{
+    static int i=0;
+    uint16_t blocks;
+    int32_t panError, tiltError;
+
+    blocks=pixy.getBlocks();
+
+    if (blocks)
+    {
+        panError=X_CENTER-pixy.blocks[0].x;
+        tiltError=pixy.blocks[0].y-Y_CENTER;
+
+        panLoop.update(panError);
+        tiltLoop.update(tiltError);
+
+        pixy.setServos(panLoop.m_pos,tiltLoop.m_pos);
+
+        i++;
+
+        if(i%50==0){
+                Serial.print(panLoop.m_pos);
+                Serial.print("\t");
+                Serial.println(tiltLoop.m_pos);
+        }
+    }
 }
 
 void loop()
 {
-	
+        //track_object();
+
+        int pwPin=6;
+        int pwPulse[5];
+	int pulse=0;
+	uint8_t pwDistance;
+
+        pinMode(pwPin,INPUT);
+	for(int i=0;i<5;i++){
+        	pwPulse[i]=pulseIn(pwPin,HIGH);
+        	pwPulse[i]=pwPulse[i]/147;
+		pulse+=pwPulse[i];
+        	delay(10);
+	}
+	pwDistance=pulse/5;
+	Serial.println(pwDistance);
 }
 
